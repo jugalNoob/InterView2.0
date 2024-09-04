9bdd425f80ab97a06138927a84b40cc3af147d22
jugal sharma <78578433+jugalNoob@users.noreply.github.com>
2024-09-04 03:46:46 -0700

    Update SearchAlgo.jsx

--- AllInterView/MERNStack/DSA/SearchAlgo.jsx
+++ AllInterView/MERNStack/DSA/SearchAlgo.jsx
@@ -1,1 +1,10 @@
 
+let data = [10, 20, 30, 40, 50, 60, 80];
+let op = 20;
+for (let i = 0; i < data.length; i++) {
+    if (data[i] > op) {
+        console.log(data[i]);
+    } else {
+        console.log("Number is greater than ",op);
+    }
+}
