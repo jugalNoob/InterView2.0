6cb08cd912d6fb6cafc972fe4409eef0c7b579b9
jugal sharma <78578433+jugalNoob@users.noreply.github.com>
2024-09-04 03:58:27 -0700

    Update SearchAlgo.jsx

--- AllInterView/MERNStack/DSA/SearchAlgo.jsx
+++ AllInterView/MERNStack/DSA/SearchAlgo.jsx
@@ -8,3 +8,70 @@ for (let i = 0; i < data.length; i++) {
         console.log("Number is greater than ",op);
     }
 }
+
+
+
+
+
+
+let obj = {
+   name: "Jugal",
+   hobbies:["walking" , "Reading"],
+   age: 25,
+   country: "India",
+   identity:{hashPanCard:false , hashAdhaarCard:true},
+   address: ["India", "USA"] // Changed {} to []
+};
+
+
+console.log(obj.hobbies[0])
+console.log(obj.name)
+console.log(obj.identity.hashPanCard)
+
+
+
+
+
+
+const objArray = [
+   {
+       index: 0,
+       details: {
+           name: "jugal",
+           rollno: 45,
+           identity:{hashPanCard:false , hashAdhaarCard:true},
+       }
+   },
+   {
+       index: 1,
+       details: {
+           name: "karan",
+           rollno: 20,
+           identity:{hashPanCard:false , hashAdhaarCard:true},
+       }
+   }
+];
+
+console.log(objArray)
+
+console.log(objArray[0])
+
+
+const objs = {
+    0: {
+        name: "jugal",
+        rollno: 45,
+        identity:{hashPanCard:false , hashAdhaarCard:true},
+    },
+    1: {
+        name: "karan",
+        rollno: 20,
+        identity:{hashPanCard:false , hashAdhaarCard:true},
+    }
+};
+
+
+
+
+
+console.log(objs)
